bd9018fa888430fa8483cc854356b4245c9d53e2
pengjie <37610029@qq.com>
2023-02-13 21:13:37 +0800

    31. 下一个排列

--- test.js
+++ test.js
@@ -1,27 +1,57 @@
 console.clear();
 
 const log = console.log;
-var nums = [2, 7, 11, 15],
-  target = 9;
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
 
 /**
  * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-var twoSum = function (nums, target) {
-  const cache = Object.create(null);
-  const l = nums.length;
-  for (let i = 0; i < l; i++) {
-    const num = nums[i];
-    const rest = target - num;
-    if (cache[rest] !== undefined) {
-      return [cache[rest], i];
-    } else {
-      cache[num] = i;
+var nextPermutation = function (nums) {
+  // 1,2,3,8,5,7,6,4
+  // 交换后
+  // 1,2,3,8,6,7,5,4
+  const swap = (i, j) => {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  };
+  for (let i = nums.length - 2; i >= 0; i--) {
+    // 找拐点
+    if (nums[i] < nums[i + 1]) {
+      for (let j = nums.length - 1; j > i; j--) {
+        if (nums[j] > nums[i]) {
+          swap(i, j);
+          break;
+        }
+      }
+      // 因为拐点之后的数组，必定是从左到右降序的
+      // 所以可以对拐点之后的数组，进行一个双指针换位
+      let start = i + 1,
+        end = nums.length - 1;
+      while (start < end) {
+        swap(start, end);
+        start++;
+        end--;
+      }
+      // 直接结束函数即可
+      return;
     }
   }
-  return [];
+  // 如果找不到，说明  nums 是从大到小排序的，则将 nums 直接 reverse 一遍
+  nums.sort((a, b) => a - b);
+  return;
 };
 
-log(twoSum(nums, target));
+var nums = [1, 2, 3];
+
+log(nextPermutation(nums));
+log(nums);
+
+// var s = "aba";
+// log(validPalindrome(s));
+
+// var s = "eeccccbebaeeabebccceea";
+// log(validPalindrome(s));
